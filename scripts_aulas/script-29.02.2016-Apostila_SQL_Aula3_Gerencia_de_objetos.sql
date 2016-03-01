31c360ceed2c7917e54020f3c874c36ff14e18c6
Lucas Basseti <lucas_basseti@hotmail.com>
2016-02-29 21:55:09 -0300

    script-29.02.2016-Apostila_SQL_Aula3_Gerencia_de_objetos

---
+++ scripts_aulas/script-29.02.2016-Apostila_SQL_Aula3_Gerencia_de_objetos.sql
@@ -0,0 +1,37 @@
+/* Apostila_SQL_Aula3_Gerencia_de_objetos.pdf */
+
+/*
+ Objetos de BD
+ 
+  DDL - Data Definition Language ou Linguagem de Definição de Dados 
+  (create, alter, drop, view, procedures)
+  
+  o principal objeto do banco de dados é a Tabela
+    dividia em Colunas e Linhas
+*/
+
+CREATE TABLE Teste3 (
+  IDTeste NUMBER NULL,  --o oracle vai colocar NOT NULL pq foi definido PRIMARY KEY(IDTeste)
+  NOME VARCHAR(40),
+  PRIMARY KEY(IDTeste)
+);
+
+
+/*
+Create Table
+– Exemplo: Criando a tabela empregado:
+*/
+
+CREATE TABLE Empregado (
+  EmpNum NUMBER NOT NULL,
+  EmpNome VARCHAR(30) NOT NULL,
+  EmpDataNasc DATE,
+  EmpServ CHAR(20),
+  EmpAdmi DATE NOT NULL,
+  EmpSalario NUMBER(10,2),
+  EmpComicao NUMBER(10,2)
+);
+
+
+
+
